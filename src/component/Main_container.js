a882e1cdcb0e09bb4f907f80bb59c26a0b8983ec
ojsung7 <ojsung7@naver.com>
2023-01-21 17:38:17 +0900

    레이아웃 정리

--- src/component/Main_container.js
+++ src/component/Main_container.js
@@ -1,9 +1,11 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 import Editor from "@monaco-editor/react";
 
 const Main_container = () => {
     const editorRef = useRef(null);
+
+    const [font_size, setFont_size] = useState('14px');
 
     function handleEditorDidMount(editor, monaco) {
         editorRef.current = editor;
@@ -15,17 +17,37 @@ const Main_container = () => {
 
     return (
         <div className='main_container'>
-            <button onClick={showValue}>Show value</button>
-            <Editor
-                height="50%"
-                defaultLanguage="c"
-                defaultValue="// some comment"
-                onMount={handleEditorDidMount}
-                options={{
-                    scrollBeyondLastLine:false,
-                    fontSize:"16px"
-                }}
-            />
+            <div className='handler'>
+                <div>
+                    <select onChange={(e) => setFont_size(e.target.value)}>
+                        <option value="">--set font size--</option>
+                        <option value="14px">14px</option>
+                        <option value="16px">16px</option>
+                        <option value="18px">18px</option>
+                        <option value="20px">20px</option>
+                    </select>
+                </div>
+                <button>play</button>
+            </div>
+            <div className='monaco_editor'>
+                <Editor
+                    height="100%"
+                    width="95%"
+                    defaultLanguage="c"
+                    defaultValue="// some comment"
+                    onMount={handleEditorDidMount}
+                    options={{
+                        scrollBeyondLastLine: false,
+                        fontSize: font_size + 'px'
+                    }}
+                    theme="vs-dark"
+                />
+            </div>
+            <div className='run_result'>
+                <div style={{ fontSize: font_size }}>
+                    여기에 결과값을 불러올 겁니다.
+                </div>
+            </div>
         </div>
     );
 }
